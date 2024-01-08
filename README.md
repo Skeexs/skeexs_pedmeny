3a8b1963a25031ac74e4cf0f8ab32551fe73f1db
Alfons <48617296+Skeexs@users.noreply.github.com>
2024-01-08 18:07:00 +0100

    Uppdaterat typ allting som går att uppdatera

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 # skeexs_pedmeny
-A Ped menu for FiveM
-This allows u to be whatever Pedastrian u want in fivem, u can either implement it to ur own personmenu or u can use a command.
-I have commented out a /ped command in the client.lua file. Just uncomment it if u want the script to be commandbased!'
-(Credits to qalle-fivem for helping out with some of the lines!)
+
+## [Hitta Dina peds här](https://docs.fivem.net/docs/game-references/ped-models/ 'Hitta Dina peds här')
--- client.lua
+++ client.lua
@@ -1,99 +1,122 @@
-local Keys = {
-    ["ESC"] = 322, ["F1"] = 288, ["F2"] = 289, ["F3"] = 170, ["F5"] = 166, ["F6"] = 167, ["F7"] = 168, ["F8"] = 169, ["F9"] = 56, ["F10"] = 57,
-    ["~"] = 243, ["1"] = 157, ["2"] = 158, ["3"] = 160, ["4"] = 164, ["5"] = 165, ["6"] = 159, ["7"] = 161, ["8"] = 162, ["9"] = 163, ["-"] = 84, ["="] = 83, ["BACKSPACE"] = 177,
-    ["TAB"] = 37, ["Q"] = 44, ["W"] = 32, ["E"] = 38, ["R"] = 45, ["T"] = 245, ["Y"] = 246, ["U"] = 303, ["P"] = 199, ["["] = 39, ["]"] = 40, ["ENTER"] = 18,
-    ["CAPS"] = 137, ["A"] = 34, ["S"] = 8, ["D"] = 9, ["F"] = 23, ["G"] = 47, ["H"] = 74, ["K"] = 311, ["L"] = 182,
-    ["LEFTSHIFT"] = 21, ["Z"] = 20, ["X"] = 73, ["C"] = 26, ["V"] = 0, ["B"] = 29, ["N"] = 249, ["M"] = 244, [","] = 82, ["."] = 81,
-    ["LEFTCTRL"] = 36, ["LEFTALT"] = 19, ["SPACE"] = 22, ["RIGHTCTRL"] = 70,
-    ["HOME"] = 213, ["PAGEUP"] = 10, ["PAGEDOWN"] = 11, ["DELETE"] = 178,
-    ["LEFT"] = 174, ["RIGHT"] = 175, ["TOP"] = 27, ["DOWN"] = 173,
-  }
+_G.exports = exports;
+--
+local ESX = exports.es_extended:getSharedObject();
+local PedMenu = {
+    currentPage = 1,
+};
 
-ESX = nil
-local PlayerData = {}
-Citizen.CreateThread(function()
-    
-    while ESX == nil do
-      TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
-      Citizen.Wait(0)
+-- * Register Command
+RegisterCommand("changeped", function(source, args)
+    local pedModel = args[1] == '' and nil or args[1];
+
+    if pedModel then
+        PedMenu:change(pedModel)
+    else
+        PedMenu:open()
     end
-  end)
-
-  RegisterNetEvent("skeexs:pedmeny")
-  AddEventHandler("skeexs:pedmeny", function()
-    OpenMenu()
 end)
 
+-- * Functions
+function PedMenu:open()
+    local elements = {
+        { label = 'Nästa sida',      value = 'nextPage' },
+        { label = 'Föregående sida', value = 'prevPage' },
+        { label = 'Din karaktär',    value = nil }
+    };
 
--- function(data, menu)
---     if data.current.value == 'ped' then
---         OpenPedMenu()
---     end,
--- 	function(data, menu)
--- 		menu.close()
--- 	end)
 
-OpenMenu = function()
-    local menuElements = {
-        {
-            ["label"] = "Välj en ped att bli.",
-            ["action"] = "choose_ped"
-        },
-        {
-            ["label"] = "Vanlig <span style = 'color:red'Karaktär</span>",
-            ["action"] = "citizen_wear"
-        }
-    }
+    for i = 1, #Config.Peds do
+        if i >= (self.currentPage * Config.PedsPerPage) - Config.PedsPerPage and i <= self.currentPage * Config.PedsPerPage then
+            table.insert(elements, {
+                label = Config.Peds[i],
+                value = Config.Peds[i]
+            })
+        end
+    end
+
+    table.insert(elements, {
+        label = 'Näsa sida',
+        value = 'nextPage'
+    })
+
+    table.insert(elements, {
+        label = 'Föregående sida',
+        value = 'prevPage'
+    })
+
 
     ESX.UI.Menu.Open("default", GetCurrentResourceName(), "ped_menu", {
-        ["title"] = "Peds",
-        ["align"] = "center",
-        ["elements"] = menuElements
-    }, function(menuData, menuHandle)
-        local action = menuData["current"]["action"]
-
-        if action == "choose_ped" then
-            ESX.UI.Menu.Open("dialog", GetCurrentResourceName(), "ped_dialog_menu", {
-                ["title"] = "Skriv in peden du vill vara."
-            }, function(menuData, menuHandle)
-                local pedModelChosen = menuData["value"]
-
-                if not type(pedModelChosen) == "number" then pedModelChosen = `pedModelChosen` end
-                    
-                if IsModelInCdimage(pedModelChosen) then
-                    while not HasModelLoaded(pedModelChosen) do
-                        Citizen.Wait(5)
-
-                        RequestModel(pedModelChosen)
-                    end
-
-                    SetPlayerModel(PlayerId(), pedModelChosen)
-                    
-                    menuHandle.close()
-                else
-                    exports.pNotify:SendNotification({text = 'Ogiltig PED', type = "error", timeout = 200, layout = "bottomCenter", queue = "center"})
-                end
-            end, function(menuData, menuHandle)
-                menuHandle.close()
-            end)
-        elseif action == "mp_m_freemode_01" then
-        
-        elseif action == "mp_f_freemode_01" then
-
-            --byt till din ped.
+        title = Config.MenuTitle,
+        align = Config.AlignMenu,
+        elements = elements
+    }, function(data, menu)
+        if data.current.value == 'nextPage' then
+            self.currentPage = self.currentPage + 1;
+            self:open()
+        elseif data.current.value == 'prevPage' then
+            self.currentPage = self.currentPage - 1;
+            self:open()
+        elseif data.current.value == nil then
+            self:change(GetEntityModel(PlayerPedId()))
+        else
+            self:change(data.current.value)
         end
-    end, function(menuData, menuHandle)
-        menuHandle.close()
+    end, function(data, menu)
+        menu.close()
     end)
 end
- 
-Citizen.CreateThread(function()
-    while true do
-        Citizen.Wait(1)
-        if IsControlJustReleased(0, Keys['F5']) then
-          exports.pNotify:SendNotification({text = 'Öppnar meny', type = "success", timeout = 200, layout = "bottomCenter", queue = "center"})  
-          Wait(500)           
-           OpenMenu()
+
+function PedMenu:search(searchQuery)
+
+end
+
+function PedMenu:change(model)
+    local chosenModel = type(model) == "string" and GetHashKey(model) or model;
+
+    if not IsModelValid(chosenModel) then
+        ESX.ShowNotification("Den här ped modellen finns inte.")
+        return;
+    end
+
+    local loaded, _model = LoadPedModel(chosenModel);
+
+    if not loaded then
+        print("Det gick inte att ladda ped modellen.", _model)
+        return
+    end
+
+    SetPlayerModel(PlayerId(), chosenModel);
+
+    local loadedModel = Find(Config.Peds, function(model)
+        return GetHashKey(model) == chosenModel
+    end)
+
+    if not loadedModel then
+        print("Det gick inte att hitta ped modellen.")
+        return
+    end
+
+    ESX.ShowNotification("Du bytte ped modell till " .. loadedModel .. ".")
+
+    SetModelAsNoLongerNeeded(chosenModel);
+
+    return true;
+end
+
+-- * Utilities
+function LoadPedModel(model)
+    while not HasModelLoaded(model) do
+        RequestModel(model)
+        Citizen.Wait(10)
+    end
+
+    return true, model
+end
+
+function Find(tbl, cb)
+    for k, v in pairs(tbl) do
+        if cb(v, k, tbl) then
+            return v, k
         end
     end
-end)
+end
